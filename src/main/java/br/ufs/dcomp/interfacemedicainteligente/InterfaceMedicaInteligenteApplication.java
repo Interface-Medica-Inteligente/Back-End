5c8d199bb3a4313331b7c92cc07765402a2ba41f
Tibuh <vitortiburtino10@gmail.com>
2021-06-08 22:47:59 -0300

    Criação do projeto

---
+++ src/main/java/br/ufs/dcomp/interfacemedicainteligente/InterfaceMedicaInteligenteApplication.java
@@ -0,0 +1,13 @@
+package br.ufs.dcomp.interfacemedicainteligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfaceMedicaInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterfaceMedicaInteligenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufs/dcomp/interfacemedicainteligente/InterfaceMedicaInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufs.dcomp.interfacemedicainteligente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterfaceMedicaInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
